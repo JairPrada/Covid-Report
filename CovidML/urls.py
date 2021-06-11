8af6859ba8a97f560f1da4b6136095231679aaee
jairprada <jairprada9@gmail.com>
2021-06-11 18:37:11 -0500

    change config

--- CovidML/urls.py
+++ CovidML/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from CovidML.views import inicio,probabilidadMapa,calcular,nosotros
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
